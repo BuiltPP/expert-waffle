242bff4a58af020d3cb903a6a161a740d2afe20a
BuiltPP <phumphawatp@gmail.com>
2017-06-04 22:31:07 +0700

    freshStart

---
+++ expert-waffle/expert-waffle/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  expert-waffle
+//
+//  Created by Phumphawat Puengya on 6/4/2560 BE.
+//  Copyright © 2560 Phumphawat Puengya. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
